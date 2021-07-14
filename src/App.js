8c4fcf55104d050300f922ba97619b7c5015f823
nabin-nath <nabinnath9@gmail.com>
2021-07-14 19:27:36 +0530

    router changed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import State from './components/State'
 import { createMuiTheme, ThemeProvider } from '@material-ui/core'
 import { red } from '@material-ui/core/colors'
